ac6e874b599bf84ff7f5e65d1b4fe45a10715040
Nicolas Cavasin <ncavasin97@gmail.com>
2021-11-09 14:32:10 -0300

    db set up and conn

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
