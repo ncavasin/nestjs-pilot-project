88a5e140b1b7c25363a7e03954a5064d6b823321
Nicolas Cavasin <ncavasin97@gmail.com>
2021-11-15 14:45:57 -0300

    reverting last push

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,11 @@
-import { Controller, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/users.entity';
-import { GrpcMethod } from '@nestjs/microservices';
 
-//@Injectable()
-@Controller()
+@Injectable()
 export class UsersService {
 
   constructor(
@@ -15,41 +13,36 @@ export class UsersService {
     private userRepository: Repository<User>
   ) { }
 
-  @GrpcMethod()
   async create(createUserDto: CreateUserDto): Promise<User> {
     console.log(`This action created user ${createUserDto.name}`);
     return await this.userRepository.save(createUserDto);
   }
 
-  @GrpcMethod()
   async findById(id: number): Promise<User> {
+    console.log(`This action found user by id: ${id}`);
     return await this.userRepository.findOne(id);
   }
 
-  @GrpcMethod()
   async findByDni(dni: number): Promise<User> {
+    console.log(`This action found user by dni: ${dni}`);
     return await this.userRepository.findOne(dni);
   }
 
-  @GrpcMethod()
   async findByName(name: string): Promise<User> {
+    console.log(`This action found user by name: ${name}`);
     return await this.userRepository.findOne(name);
   }
 
-  @GrpcMethod()
   async findAll(): Promise<User[]> {
     return await this.userRepository.find();
   }
 
-  @GrpcMethod()
   async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<void> {
     console.log(`This action updated user #${id}`);
     await this.userRepository.update(id, updateUserDto);
     return
-
   }
 
-  @GrpcMethod()
   async deleteUser(id: number): Promise<void> {
     console.log(`This action removed user #${id}`)
     await this.userRepository.delete(id);
