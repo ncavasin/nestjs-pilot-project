464797e99f13db0cf2f5b6a0d2a0c0574a4e15e7
Nicolas Cavasin <ncavasin97@gmail.com>
2021-11-15 14:46:49 -0300

    adding mapping with grpc tags

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Body, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { GrpcMethod } from '@nestjs/microservices';
+import { User } from './entities/users.entity';
 
-@Controller('users')
+@Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  //@Post()
+  @GrpcMethod()
+  create(@Body() createUserDto: CreateUserDto){
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
+  //@Get(':id')
+
+  @GrpcMethod()
+  findById(@Param('id') id: string) {
+    return this.usersService.findById(+id);
+  }
+  @GrpcMethod()
+  findByDni(dni: number): Promise<User> {
+    return this.usersService.findByDni(dni);
+  }
+
+  @GrpcMethod()
+  findByName(name: string): Promise<User> {
+    return this.usersService.findByName(name);
+  }
+
+  //@Get()
+  @GrpcMethod()
   findAll() {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findById(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //@Patch(':id')
+  @GrpcMethod()
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.updateUser(+id, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  //@Delete(':id')
+  @GrpcMethod()
+  deleteUser(@Param('id') id: string) {
     return this.usersService.deleteUser(+id);
   }
 }
