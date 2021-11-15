b5f87a9f0709ead67ff285664efcf2d06d702502
Nicolas Cavasin <ncavasin97@gmail.com>
2021-11-15 14:45:02 -0300

    adding microservice to work with grpc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+const microserviceOptions = {
+  transport: Transport.GRPC,
+  options: {
+    package: 'users',
+    protoPath: join(process.cwd(), '/src/users/users.proto'),
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
+  await app.listen();
 }
 bootstrap();
